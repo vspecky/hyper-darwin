9dac7c1722ab9b2e6a0d4df2f7765e9530552379
vspecky <thelonenerd9913@gmail.com>
2020-08-14 11:50:29 +0530

    Implemented Random Distribution for Activations

--- src/activations.rs
+++ src/activations.rs
@@ -1,4 +1,5 @@
-use rand::{Rand, Rng};
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
 use std::cmp::PartialEq;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -11,15 +12,15 @@ pub enum Activations {
     Linear,
 }
 
-impl Rand for Activations {
-    fn rand<R: Rand>(rng: &mut R) -> Self {
+impl Distribution<Activations> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Activations {
         match rng.gen_range(0, 6) {
-            0 => Self::Sine,
-            1 => Self::Cosine,
-            3 => Self::Gaussian,
-            4 => Self::Sigmoid,
-            5 => Self::Absolute,
-            6 => Self::Linear,
+            0 => Activations::Sine,
+            1 => Activations::Cosine,
+            3 => Activations::Gaussian,
+            4 => Activations::Sigmoid,
+            5 => Activations::Absolute,
+            6 => Activations::Linear,
         }
     }
 }
