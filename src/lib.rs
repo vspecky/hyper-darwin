d9f7a805788d556d303d556b614ca9da52cc9ad4
vspecky <thelonenerd9913@gmail.com>
2020-08-26 17:14:28 +0530

    Imports and public exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,13 @@ mod activations;
 mod connection;
 mod genome;
 mod history;
+mod hyper_tensor;
 mod node;
 mod population;
 mod settings;
 mod species;
 
+pub use hyper_tensor::HyperTensor;
 pub use population::Population;
+pub use settings::HyperSettings;
 pub use settings::Settings;
